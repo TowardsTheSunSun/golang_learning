e190d28a43da33524b7611ea7822e3e7d146c2bf
poplar89 <techyangcy@gmail.com>
2021-01-04 21:17:58 +0800

    一些go例子

--- src/cc/yangcy/stringutil/reverse.go
+++ src/cc/yangcy/stringutil/reverse.go
@@ -1,5 +1,8 @@
 package stringutil
 
+const AnExportBlankString = " "
+const anotherBlankString = "\t"
+
 func Reverse(src string) string {
 	r := []rune(src)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
--- src/client/main.go
+++ src/client/main.go
@@ -5,11 +5,38 @@ import (
 	"flag"
 	"fmt"
 	"github.com/golang/glog"
+	"math/rand"
 )
 
+const pi = 3.141592653
+
 func main() {
+	//glog使用
 	flag.Parse()
-	fmt.Printf("1234567 ==> %v\n", stringutil.Reverse("1234567"))
 	glog.Info("This is the first log from ")
 	glog.Flush()
+	//自己的module使用
+	fmt.Printf("1234567 ==> %v\n", stringutil.Reverse("1234567"))
+	//使用math/rand
+	fmt.Printf("rand.Int31n(): %v\n", rand.Int31n(100))
+	//使用本类中定义的函数
+	fmt.Printf("1+2=%v", add(1, 2))
+	//返回两个结果
+	a, b := swap(1, 2)
+	fmt.Printf("1 , 2 swap %v , %v\n", a, b)
+	fmt.Println(swap(1, 2))
+	//使用常量
+	fmt.Printf("Pi constant is %v\n", pi)
+	//cannot refer to unexported name stringutil.blankString
+	//fmt.Printf("stringutil.blankString constant is \"%v\"", stringutil.anotherBlankString)
+	//大些变量默认导出
+	fmt.Printf("stringutil.AnExportBlankString constant is \"%v\"\n", stringutil.AnExportBlankString)
+}
+
+func add(a, b int) int {
+	return a + b
+}
+
+func swap(a, b int) (int, int) {
+	return b, a
 }
